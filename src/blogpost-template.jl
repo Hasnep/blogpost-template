6fb509566cd99119ac57d08c8c577a99cf53d2f8
Hannes <h@nnes.dev>
2022-11-21 17:26:57 +0000

    Add more content to test blogpost

--- src/blogpost-template.jl
+++ src/blogpost-template.jl
@@ -1,1 +1,13 @@
 # This is a blogpost template.
+
+# Newton's second law says that $F \propto a$.
+
+F(m, a) = m * a
+
+# The acceleration due to gravity on earth is $9.81 ms^{-2}$
+
+g = 9.81
+
+# So a $100 g$ apple exerts this much force due to gravity
+
+F(0.1, g)
