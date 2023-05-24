b2f8407aa23f304fb295b57292bffcb48e511765
Hannes <h@nnes.dev>
2023-05-24 12:28:26 +0800

    Add drawing

--- src/blogpost-template.jl
+++ src/blogpost-template.jl
@@ -2,7 +2,7 @@
 
 # Newton's second law says that $F \propto a$.
 
-F(m, a) = m * a
+F(m, a) = m * a;
 
 # The acceleration due to gravity on earth is $9.81 ms^{-2}$
 
@@ -11,3 +11,11 @@ g = 9.81
 # So a $100 g$ apple exerts this much force due to gravity
 
 F(0.1, g)
+
+# Testing drawing
+
+using Luxor
+
+@drawsvg begin
+    juliacircles()
+end
